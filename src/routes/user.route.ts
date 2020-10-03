c666001a61e55e2b836bc721d94978b7c486172c
steelvelveteen <joeyvico@gmail.com>
2020-10-04 10:13:40 +1030

    Cors

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,12 +2,13 @@ import express, { Router } from 'express';
 import * as userController from '../controllers/user.controller';
 // import { checkAuth } from '../middleware/check-auth.middleware';
 import { check } from 'express-validator';
+import cors from 'cors';
 
 const router: Router = express.Router();
 
 // router.get('/', checkAuth, getAllUsers);
 // router.get('/', checkAuth, userController.getAllUsers);
-router.get('/', userController.getAllUsers);
+router.get('/', cors(), userController.getAllUsers);
 
 router.post(
     '/signup',
