bfe02d2d3d7c7cbda38e92b912262fd412f31ff7
steelvelveteen <joeyvico@gmail.com>
2020-10-05 17:15:01 +1030

    Renamed register to signup in controller. Fixed cors shit. Was the browser keeping me behind

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,17 +1,15 @@
 import express, { Router } from 'express';
 import * as userController from '../controllers/user.controller';
-// import { checkAuth } from '../middleware/check-auth.middleware';
+import { checkAuth } from '../middleware/check-auth.middleware';
 import { check } from 'express-validator';
 import cors from 'cors';
 
 const router: Router = express.Router();
 
-// router.get('/', checkAuth, getAllUsers);
-// router.get('/', checkAuth, userController.getAllUsers);
-router.get('/', cors(), userController.getAllUsers);
+router.get('/', cors(), checkAuth, userController.getAllUsers);
 
 router.post(
-    '/register',
+    '/signup',
     [check('email').isEmail(), check('password').isLength({ min: 8 })],
     userController.signup
 );
