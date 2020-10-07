99a08177b4800a5741a587a0c4efd636739892a1
steelvelveteen <joeyvico@gmail.com>
2020-10-07 17:02:56 +1030

    Clean up

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,21 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import indexRouter from './routes/index';
 import userRouter from './routes/user.route';
-// import loginRouter from './routes/login.route';
-// import accountRouter from './routes/account.route';
 
 export const app: Express = express();
-// export const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-/** Imported routes */
 
+/** Imported routes */
 app.use(morgan('dev'));
 app.use('/', indexRouter); // This is here for testing alone
 app.use('/users', userRouter);
-// app.use('/login', loginRouter);
-// app.use('/accounts', accountRouter);
 
 /** Error handling */
 interface ResponseError extends Error {
-    status?: number; /** So I can add the status property to the Error object. */
+    /** So I can add the status property to the Error object. */
+    status?: number;
 }
 
 app.use((_: Request, res: Response, next: NextFunction) => {
