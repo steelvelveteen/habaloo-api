122a515d235c2c2596a1b7eb5e02f01a7ddc0e28
steelvelveteen <joeyvico@gmail.com>
2020-10-09 12:22:56 +1030

    Reverted checkAuth, docker token problem and heroku cors issue

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,14 +2,14 @@ import express, { Router } from 'express';
 import * as userController from '../controllers/user.controller';
 import * as loginController from '../controllers/login.controller';
 import * as accountController from '../controllers/account.controller';
-import { checkAuth } from '../middleware/check-auth.middleware';
+// import { checkAuth } from '../middleware/check-auth.middleware';
 import { check } from 'express-validator';
 import cors from 'cors';
 
 const router: Router = express.Router();
 
-router.get('/', cors(), checkAuth, userController.getAllUsers);
-// router.get('/', cors(), userController.getAllUsers);
+// router.get('/', cors(), checkAuth, userController.getAllUsers);
+router.get('/', cors(), userController.getAllUsers);
 
 router.post(
     '/signup',
