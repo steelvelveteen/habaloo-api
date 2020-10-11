15cb488553b53a7ee6168fc7ebf4d62cca99c10a
steelvelveteen <joeyvico@gmail.com>
2020-10-12 09:14:24 +1030

    Attempting to dockerize again, fixing  the split problem for the auth token

--- src/middleware/check-auth.middleware.ts
+++ src/middleware/check-auth.middleware.ts
@@ -4,9 +4,14 @@ require('dotenv/config');
 
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1];
-        const KEY = process.env.ACCESS_TOKEN_SECRET;
-        jwt.verify(token!, KEY as Secret);
+        // const token = req.headers.authorization?.split(' ')[1];
+        const auth = req.headers['authorization'];
+        if (auth) {
+            const token = auth.split(' ')[1];
+            const KEY = process.env.ACCESS_TOKEN_SECRET;
+            jwt.verify(token!, KEY as Secret);
+        }
+
     } catch (error) {
         res.status(403).json({
             message: 'Unauthorized',
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,13 @@
 import express, { Router } from 'express';
 import * as userController from '../controllers/user.controller';
 import * as accountController from '../controllers/account.controller';
-// import { checkAuth } from '../middleware/check-auth.middleware';
+import { checkAuth } from '../middleware/check-auth.middleware';
 import cors from 'cors';
 
 const router: Router = express.Router();
 
-// router.get('/', cors(), checkAuth, userController.getAllUsers);
-router.get('/', cors(), userController.getAllUsers);
+router.get('/', cors(), checkAuth, userController.getAllUsers);
+// router.get('/', cors(), userController.getAllUsers);
 
 router.post('/createaccount', accountController.createaccount);
 
